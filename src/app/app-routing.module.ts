5b8c03a7d108897ecafb886453f75ffeff2ed37c
RatingRishu <rairishabh5001@gmail.com>
2024-04-22 10:45:36 +0530

    update UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse/browse.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'browse', component: BrowseComponent },
+  { path: 'login', component: LoginComponent }, 
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
 
 @NgModule({
